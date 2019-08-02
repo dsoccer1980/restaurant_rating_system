1444d34183ae49a2fe950131ed5ee4f248e43543
dsoccer1980 <dsoccer1980@gmail.com>
2019-08-02 18:47:46 +0300

    Add Controller tests

---
+++ src/test/java/ru/dsoccer1980/web/UserRestControllerTest.java
@@ -0,0 +1,38 @@
+package ru.dsoccer1980.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.dsoccer1980.repository.UserRepository;
+import ru.dsoccer1980.service.RoleService;
+import ru.dsoccer1980.service.UserService;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest({UserRestController.class})
+@MockBean(classes = {UserService.class, RoleService.class, UserRepository.class})
+class UserRestControllerTest extends AbstractControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void saveUser() throws Exception {
+        mvc.perform(post("/user")
+                .contentType(APPLICATION_JSON)
+                .content("{\"name\":\"new\", \"email\":\"new@g.ee\", \"password\":\"password\"}"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void saveCompany() throws Exception {
+        mvc.perform(post("/company")
+                .contentType(APPLICATION_JSON)
+                .content("{\"name\":\"new\", \"email\":\"new@g.ee\", \"password\":\"password\"}"))
+                .andExpect(status().isOk());
+    }
+}
