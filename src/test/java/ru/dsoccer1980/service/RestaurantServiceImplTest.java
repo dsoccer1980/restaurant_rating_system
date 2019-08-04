56bc73f99632e6765975aa2bd459f72352ac4ca9
dsoccer1980 <dsoccer1980@gmail.com>
2019-08-04 14:17:50 +0300

    Refactor RestaurantServiceImplTest with Mock repository

--- src/test/java/ru/dsoccer1980/service/RestaurantServiceImplTest.java
+++ src/test/java/ru/dsoccer1980/service/RestaurantServiceImplTest.java
@@ -1,80 +1,94 @@
 package ru.dsoccer1980.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.dsoccer1980.model.Restaurant;
 import ru.dsoccer1980.model.Role;
 import ru.dsoccer1980.model.User;
+import ru.dsoccer1980.repository.RestaurantRepository;
 import ru.dsoccer1980.util.config.InitProps;
 import ru.dsoccer1980.util.exception.NotFoundException;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
-@AutoConfigureTestEntityManager
-class RestaurantServiceImplTest extends AbstractServiceTest {
+@ExtendWith(SpringExtension.class)
+@Import({RestaurantServiceImpl.class})
+class RestaurantServiceImplTest {
 
     private final Role ROLE_COMPANY = new Role(40L, InitProps.ROLE_COMPANY);
     private final User USER1 = new User(1L, "Ivanov", "ivan@gmail.com", "password",
             LocalDateTime.of(2019, 7, 31, 0, 0, 0), Set.of(ROLE_COMPANY));
-    private final User USER2 = new User(2L, "Petrov", "petr@gmail.com", "password2",
-            LocalDateTime.of(2019, 7, 31, 0, 0, 0), Set.of(ROLE_COMPANY));
 
     private final Restaurant RESTAURANT1 = new Restaurant(10L, "TSAR", "Nevskij 53", USER1);
-    private final Restaurant RESTAURANT2 = new Restaurant(11L, "Europe", "Mihailovskaja 14", USER2);
 
-    @Autowired
-    TestEntityManager testEntityManager;
     @Autowired
     private RestaurantService service;
 
+    @MockBean
+    private RestaurantRepository restaurantRepository;
+
     @Test
     void get() {
+        when(restaurantRepository.findById(RESTAURANT1.getId())).thenReturn(Optional.of(RESTAURANT1));
         assertThat(service.get(RESTAURANT1.getId())).isEqualTo(RESTAURANT1);
     }
 
     @Test
     void getNotExist() {
+        when(restaurantRepository.findById(-1L)).thenReturn(Optional.empty());
         assertThrows(NotFoundException.class, () -> service.get(-1));
     }
 
     @Test
-    @Transactional
     void create() {
-        User newUser = testEntityManager.persist(new User("new", "new@gmail.com", "password3", Set.of(ROLE_COMPANY)));
+        User newUser = new User("new", "new@gmail.com", "password3", Set.of(ROLE_COMPANY));
+        Restaurant savedRestaurant = new Restaurant(100L, "New", "Address", newUser);
+        when(restaurantRepository.save(new Restaurant("New", "Address", newUser))).thenReturn(savedRestaurant);
+
         Restaurant newRestaurant = service.create(new Restaurant("New", "Address", newUser));
-        assertThat(service.get(newRestaurant.getId())).isEqualTo(newRestaurant);
+        assertThat(newRestaurant).isEqualTo(savedRestaurant);
     }
 
     @Test
     void update() {
-        Restaurant restaurantForUpdate = service.get(RESTAURANT1.getId());
-        restaurantForUpdate.setAddress("Update address");
-        service.update(restaurantForUpdate);
+        User newUser = new User("new", "new@gmail.com", "password3", Set.of(ROLE_COMPANY));
+        Restaurant updatedRestaurant = new Restaurant(100L, "Updated", "Address", newUser);
+        when(restaurantRepository.save(new Restaurant(100L, "New", "Address", newUser))).thenReturn(updatedRestaurant);
 
-        assertThat(service.get(RESTAURANT1.getId())).isEqualTo(restaurantForUpdate);
+        assertThat(service.update(new Restaurant(100L, "New", "Address", newUser))).isEqualTo(updatedRestaurant);
     }
 
     @Test
     void delete() {
+        doNothing().when(restaurantRepository).deleteById(RESTAURANT1.getId());
+        when(restaurantRepository.findById(RESTAURANT1.getId())).thenThrow(new NotFoundException("not found"));
+
         service.delete(RESTAURANT1.getId());
+        verify(restaurantRepository, times(1)).deleteById(RESTAURANT1.getId());
         assertThrows(NotFoundException.class, () -> service.get(RESTAURANT1.getId()));
     }
 
     @Test
     void deleteWithWrongId() {
+        doThrow(EmptyResultDataAccessException.class).when(restaurantRepository).deleteById(-1L);
         assertThrows(NotFoundException.class, () -> service.delete(-1));
     }
 
     @Test
     void getRestaurantByUserId() {
-        Restaurant restaurant = service.getRestaurantByUserId(USER1.getId());
-        assertThat(restaurant).isEqualTo(RESTAURANT1);
+        when(restaurantRepository.findRestaurantByUserId(USER1.getId())).thenReturn(RESTAURANT1);
+
+        assertThat(service.getRestaurantByUserId(USER1.getId())).isEqualTo(RESTAURANT1);
     }
 }
