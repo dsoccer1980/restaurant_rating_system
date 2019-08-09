bcc6c4e572431b8edef84ff656bbbad56bccbbd0
dsoccer1980 <dsoccer1980@gmail.com>
2019-08-09 15:45:25 +0300

    Refactor

--- src/main/java/ru/dsoccer1980/security/JwtTokenUtil.java
+++ src/main/java/ru/dsoccer1980/security/JwtTokenUtil.java
@@ -7,7 +7,6 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.impl.DefaultClock;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.io.Serializable;
