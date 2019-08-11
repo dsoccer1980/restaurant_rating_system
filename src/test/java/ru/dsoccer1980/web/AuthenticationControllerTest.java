aaa1371ddad98cb95b3f0bb2bc0764bf4af4415e
dsoccer1980 <dsoccer1980@gmail.com>
2019-08-11 15:22:03 +0300

    Refactor AuthenticationControllerTest

--- src/test/java/ru/dsoccer1980/web/AuthenticationControllerTest.java
+++ src/test/java/ru/dsoccer1980/web/AuthenticationControllerTest.java
@@ -5,16 +5,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.dsoccer1980.model.Role;
 import ru.dsoccer1980.model.User;
 import ru.dsoccer1980.repository.UserRepository;
 import ru.dsoccer1980.security.JwtTokenRequest;
+import ru.dsoccer1980.util.config.InitProps;
 
+import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.Optional;
+import java.util.Set;
 
 import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -25,11 +31,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest({AuthenticationController.class})
 public class AuthenticationControllerTest extends AbstractControllerTest {
 
+    private final Role ROLE_USER = new Role(40L, InitProps.ROLE_USER);
+    private final Role ROLE_COMPANY = new Role(41L, InitProps.ROLE_COMPANY);
+    private final User USER1 = new User(1L, "user", "ivan@gmail.com", "password", LocalDateTime.now(), Set.of(ROLE_USER));
+    private final User COMPANY = new User(2L, "company", "petr@gmail.com", "password2", LocalDateTime.now(), Set.of(ROLE_COMPANY));
+
     @Autowired
     private MockMvc mvc;
 
     @MockBean
     private UserRepository userRepository;
+
+    @PostConstruct
+    void postConstruct() {
+        given(userRepository.findByName("company")).willReturn(Optional.of(COMPANY));
+        given(userRepository.findByName("user")).willReturn(Optional.of(USER1));
+    }
 
     @Test
     void testAuthenticateUser() throws Exception {
@@ -54,7 +71,7 @@ public class AuthenticationControllerTest extends AbstractControllerTest {
     }
 
     @Test
-    @WithMockUser(username = "company", authorities = {"ROLE_COMPANY"})
+    @WithUserDetails(value = "company")
     void getRoleWithCompany() throws Exception {
         mvc.perform(get("/role").param("currentUser", "{\"name\":\"user1\"}"))
                 .andExpect(status().isOk())
@@ -62,7 +79,7 @@ public class AuthenticationControllerTest extends AbstractControllerTest {
     }
 
     @Test
-    @WithMockUser(username = "user", authorities = {"ROLE_USER"})
+    @WithUserDetails(value = "user")
     void getRoleWithUser() throws Exception {
         mvc.perform(get("/role").param("currentUser", "{\"name\":\"user1\"}"))
                 .andExpect(status().isOk())
